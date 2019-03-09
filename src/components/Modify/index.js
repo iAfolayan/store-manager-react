8aa27b76b29b7ae9b427858df680e9f06d8b4d81
Isaiah Afolayan <isaiahafolayan@Isaiahs-MacBook-Pro.local>
2019-03-09 17:27:15 +0100

    [ft-#164192664] fix list products

--- src/components/Modify/index.js
+++ src/components/Modify/index.js
@@ -1,88 +1,84 @@
-import React, { Component, Fragment } from 'react';
-import InputForm from '../../components/InputForms';
-import { CreateProductValidator } from '../../helpers/validate';
+import React, { Component, Fragment} from 'react';
+import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import prodImage5 from '../../images/5.PNG';
 import './Modify.scss';
+import { Pagination } from 'reactstrap';
+
+function mapStateToProps(state) {
+  return {
+
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+
+  };
+}
 
 class Modify extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      product: {
-        name: '',
-        category: '',
-        description:'',
-        price:'',
-        minimumAllowed:''
-      },
-      isLoading: false,
-      errors: {}
-    };
-  }
 
-  handleChange = event => {
-    const { product } = this.state;
-    const { name, value } = event.target;
-    product[name] = value;
-    this.setState({
-      product
-    });
-  }
-
-  handleSelect = event => {
-    const { product } = this.state;
-    const { value } = event.target.options[event.target.selectedIndex];
-    article.category = value;
-    this.setState({ product });
-  }
-
-  handleSubmit = event => {
-    event.preventDefault();
-    const { product } = this.state;
-    const errors = CreateProductValidator(product);
-    if (errors) {
-      this.setState({ errors });
-    }
-    // const { login } = this.props;
-    // login(product);
-  }
-			
   render() {
-    const { product, errors, isLoading } = this.state;
     return (
-      <Fragment>
-        <h5 className="ml-3 text-primary">Modify Product</h5>
+     <Fragment>
+        <h5 className="ml-3 text-primary">Products</h5>
         <hr className="mr-5" />
-        <div className="col-md-6 formWrapper">
-          <form onSubmit={this.handleSubmit}>
-            <InputForm text="Name" error={errors.name} label="productName" type="text" name="productName" id="productName" value="blueetooth" placeholder="Name" onChange={this.handleChange} />
-            <small className="form-text text-muted">Product name is required.</small>
-            <div className='form-group'>
-              <label htmlFor="category" className="text-sm-left">Category</label>
-              <select className="form-control" name="category" onChange={this.handleSelect}>
-                <option value="" disabled>Select a category</option>
-                <option value="Mobile">Mobile</option>
-              </select>
-            </div>
-            <div className="form-group">
-              <label htmlFor="description" className="text-sm-left">Description</label>
-              <textarea className="form-control" name="description" id="description" placeholder="Description" onChange={this.handleChange}>New technology </textarea>
-              <small id="descHelp" className="form-text text-muted">Give a descriptive information about product</small>
-              <span className="text-danger">{errors.description}</span>
-            </div>
-            <InputForm text="Price" error={errors.price} label="price" type="text" name="price" id="price" value={product.price} placeholder="Enter price" onChange={this.handleChange} />
-            <InputForm text="Minimun Sales Allowed" error={errors.minimumAllowed} label="minimumAllowed" type="text" name="minimumAllowed" id="minimumAllowed" value={product.minimumAllowed} placeholder="Enter minimum sales allowed" onChange={this.handleChange} />
-            <div className="form-group">
-              <label htmlFor="image text-sm-left">Image</label>
-              <input type="file" className="form-control-file" id="image" />
-              <small id="productHelp" className="form-text text-muted">Change product Image</small>
-              <span className="text-danger">{errors.image}</span>
-            </div>
-            <button className="btn btn-outline-primary float-right">{ isLoading ? 'Update product' : 'Loading...'}</button>
-          </form>
-        </div>
-        </Fragment>
+      <div className="table-responsive-sm">
+        <table className="table table-bordered table-hover">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">category</th>
+              <th scope="col">price</th>
+              <th scope="col">Image</th>
+              <th scope="col">ACTION</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Bluetooth</td>
+              <td>Phone</td>
+              <td>3000</td>
+              <td>
+                <img src={prodImage5} className="rounded mx-auto d-block" alt="image title" />
+              </td>
+              <td>
+                <NavLink to="edit">Edit</NavLink> | Delete</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Bluetooth</td>
+              <td>Phone</td>
+              <td>3000</td>
+              <td>
+                <img src={prodImage5} className="rounded mx-auto d-block" alt="image title" />
+              </td>
+              <td>
+                <NavLink to="edit">Edit</NavLink> | Delete</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Bluetooth</td>
+              <td>Phone</td>
+              <td>3000</td>
+              <td>
+                <img src={prodImage5} className="rounded mx-auto d-block" alt="image title" />
+              </td>
+              <td>
+                <NavLink to="edit">Edit</NavLink> | Delete</td>
+            </tr>
+            
+          </tbody>
+        </table>
+      </div>
+    </Fragment>
     );
   }
 }
 
-export default Modify;
+export default connect(
+  mapStateToProps,
+)(Modify);
