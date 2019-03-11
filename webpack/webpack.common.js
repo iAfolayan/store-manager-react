f61d2d60b2c9e0369e969f608cd2b1be58180ce4
Isaiah Afolayan <isaiahafolayan@Isaiahs-MacBook-Pro.local>
2019-03-11 18:20:20 +0100

    heroku

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,39 +1,83 @@
-const webpack = require('webpack');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: ['babel-polyfill', './src/index.js'],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
-        loader: 'url-loader?limit=100000',
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx'],
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
-    }),
-  ],
+ entry: {
+   app: './src/index.jsx'
+ },
+ output: {
+   filename: 'app.bundle.js',
+   path: path.resolve(__dirname, '../dist'),
+   publicPath: '/'
+ },
+ module: {
+   rules: [
+     {
+       test: /\.(js|jsx)$/,
+       exclude: /node_modules/,
+       use: {
+         loader: 'babel-loader'
+       }
+     },
+     {
+       test: /\.html$/,
+       use: [
+         {
+           loader: 'html-loader',
+           options: {
+             minimize: true
+           }
+         }
+       ]
+     },
+     {
+       test: /\.(png|PNG|woff|woff2|eot|ttf|svg)$/,
+       loader: 'url-loader'
+     },
+     {
+       test: /\.(css|scss)$/,
+       use: [
+         MiniCssExtractPlugin.loader,
+         'css-loader',
+         'postcss-loader',
+         'sass-loader'
+       ]
+     },
+     {
+       test: /\.(png|PNG|jpg)$/,
+       include: path.join(__dirname, 'static/images'),
+       loader: 'file-loader'
+     },
+     {
+       test: /\.(gif|png|PNG|jpe?g|svg)$/i,
+       use: [
+         'file-loader',
+         {
+           loader: 'image-webpack-loader',
+           options: {
+             bypassOnDebug: true,
+             disable: true
+           }
+         }
+       ]
+     }
+   ]
+ },
+ resolve: {
+   extensions: ['.js', '.jsx']
+ },
+ devServer: {
+   historyApiFallback: true
+ },
+ plugins: [
+   new HtmlWebpackPlugin({
+     title: 'Store',
+     template: './index.html'
+   }),
+   new MiniCssExtractPlugin({
+     filename: '[name].css',
+     chunkFilename: '[id].css'
+   })
+ ]
 };
