e5fefbe2b8516ad78a261e17f28fd2d6fdbe698b
Isaiah Afolayan <isaiahafolayan@Isaiahs-MacBook-Pro.local>
2019-03-11 17:48:11 +0100

    [heroku fix]

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,66 +1,83 @@
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: {
-    app: './src/index.jsx'
-  },
-  output: {
-    filename: 'app.bundle.js',
-    path: path.resolve(__dirname, '../dist'),
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: {
-              minimize: true
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(jpg|png|PNG|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader'
-      },
-      {
-        test: /\.(css|scss)$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
-      },
-      {
-        test: /.*\.(gif|png|PNG|jpg|svg)$/i,
-        include: path.join(__dirname, 'static/images'),
-        loader: 'file-loader'
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Store Manager',
-      template: './index.html'
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
-    })
-  ]
+ entry: {
+   app: './src/index.jsx'
+ },
+ output: {
+   filename: 'app.bundle.js',
+   path: path.resolve(__dirname, '../dist'),
+   publicPath: '/'
+ },
+ module: {
+   rules: [
+     {
+       test: /\.(js|jsx)$/,
+       exclude: /node_modules/,
+       use: {
+         loader: 'babel-loader'
+       }
+     },
+     {
+       test: /\.html$/,
+       use: [
+         {
+           loader: 'html-loader',
+           options: {
+             minimize: true
+           }
+         }
+       ]
+     },
+     {
+       test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+       loader: 'url-loader'
+     },
+     {
+       test: /\.(css|scss)$/,
+       use: [
+         MiniCssExtractPlugin.loader,
+         'css-loader',
+         'postcss-loader',
+         'sass-loader'
+       ]
+     },
+     {
+       test: /\.(png|jpg)$/,
+       include: path.join(__dirname, 'static/images'),
+       loader: 'file-loader'
+     },
+     {
+       test: /\.(gif|png|jpe?g|svg)$/i,
+       use: [
+         'file-loader',
+         {
+           loader: 'image-webpack-loader',
+           options: {
+             bypassOnDebug: true,
+             disable: true
+           }
+         }
+       ]
+     }
+   ]
+ },
+ resolve: {
+   extensions: ['.js', '.jsx']
+ },
+ devServer: {
+   historyApiFallback: true
+ },
+ plugins: [
+   new HtmlWebpackPlugin({
+     title: 'Store Manager',
+     template: './index.html'
+   }),
+   new MiniCssExtractPlugin({
+     filename: '[name].css',
+     chunkFilename: '[id].css'
+   })
+ ]
 };
